7e175b0579f35bfe5688ad441db49a60971b9fe5
Dardler <Dardler@users.noreply.github.com>
2013-08-19 16:07:47 +0400

    Update ip.php

--- ip.php
+++ ip.php
@@ -4,8 +4,8 @@ curl_setopt($ch, CURLOPT_URL, "http://ipv4.internet.yandex.ru/api/v0/ip");
 curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.64 Safari/537.31");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_REFERER, "https://www.google.ru/");
-curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 20);
+//curl_setopt($ch, CURLOPT_POST, true);
+//curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 20);
 
 $output = curl_exec($ch);
 $info = curl_getinfo($ch);
