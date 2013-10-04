f5eb5d37c68b62496ee4263b4698655c474c00fa
Dardler <Dardler@users.noreply.github.com>
2013-10-04 23:59:14 +0400

    Update ip2.php

--- ip2.php
+++ ip2.php
@@ -1,5 +1,16 @@
 <?php
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, "http://ipv4.internet.yandex.ru/api/v0/ip");
+curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.64 Safari/537.31");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_REFERER, "https://www.google.ru/");
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 20);
 
-$MyIP = file_get_contents('http://ipv4.internet.yandex.ru/api/v0/ip');
-echo $MyIP . "</br>";
+$output = curl_exec($ch);
+$info = curl_getinfo($ch);
+$outback[0] = $info['http_code'];
+$outback[1] = $info['total_time'];
+var_dump($outback);
+echo $output;
 ?>
