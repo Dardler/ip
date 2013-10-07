2343063efd14f6549e424948cdc55a4cd521f7f9
Dardler <Dardler@users.noreply.github.com>
2013-10-07 22:36:29 +0400

    Update A.php

--- A.php
+++ A.php
@@ -1,4 +1,120 @@
 <?php
 
+$newURL = ($_GET['U']);
+$sFM = ($_GET['F']);
+$iDnum = ($_GET['N']);
+$sDDay = ($_GET['D']);
+$sDMonth = ($_GET['M']);
+$sDYear = ($_GET['Y']);
+$sCountry = ($_GET['C']);
+
+$ip1 = rand (1, 254);
+$ip2 = rand (1, 254);
+$ip3 = rand (1, 254);
+$ip4 = rand (1, 254);
+$mainProxy  = "$ip1.$ip2.$ip3.$ip4";
+
+$UaArr[]='Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/525.19 (KHTML, like Gecko) Chrome/1.0.154.53 Safari/525.19';
+$UaArr[]='Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/525.19 (KHTML, like Gecko) Chrome/1.0.154.36 Safari/525.19';
+$UaArr[]='Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/534.10 (KHTML, like Gecko) Chrome/7.0.540.0 Safari/534.10';
+$UaArr[]='Mozilla/5.0 (Windows; U; Windows NT 5.2; en-US) AppleWebKit/534.4 (KHTML, like Gecko) Chrome/6.0.481.0 Safari/534.4';
+$UaArr[]='Mozilla/5.0 (Macintosh; U; Intel Mac OS X; en-US) AppleWebKit/533.4 (KHTML, like Gecko) Chrome/5.0.375.86 Safari/533.4';
+$UaArr[]='Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/532.2 (KHTML, like Gecko) Chrome/4.0.223.3 Safari/532.2';
+$UaArr[]='Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/532.0 (KHTML, like Gecko) Chrome/4.0.201.1 Safari/532.0';
+$UaArr[]='Mozilla/5.0 (Windows; U; Windows NT 5.2; en-US) AppleWebKit/532.0 (KHTML, like Gecko) Chrome/3.0.195.27 Safari/532.0';
+$UaArr[]='Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/530.5 (KHTML, like Gecko) Chrome/2.0.173.1 Safari/530.5';
+$UaArr[]='Mozilla/5.0 (Windows; U; Windows NT 5.2; en-US) AppleWebKit/534.10 (KHTML, like Gecko) Chrome/8.0.558.0 Safari/534.10';
+$UaArr[]='Mozilla/5.0 (X11; U; Linux x86_64; en-US) AppleWebKit/540.0 (KHTML,like Gecko) Chrome/9.1.0.0 Safari/540.0';
+$UaArr[]='Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/534.14 (KHTML, like Gecko) Chrome/9.0.600.0 Safari/534.14';
+$UaArr[]='Mozilla/5.0 (X11; U; Windows NT 6; en-US) AppleWebKit/534.12 (KHTML, like Gecko) Chrome/9.0.587.0 Safari/534.12';
+$UaArr[]='Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/534.13 (KHTML, like Gecko) Chrome/9.0.597.0 Safari/534.13';
+$UaArr[]='Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/534.16 (KHTML, like Gecko) Chrome/10.0.648.11 Safari/534.16';
+$UaArr[]='Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US) AppleWebKit/534.20 (KHTML, like Gecko) Chrome/11.0.672.2 Safari/534.20';
+$UaArr[]='Mozilla/5.0 (Windows NT 6.0) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/14.0.792.0 Safari/535.1';
+$UaArr[]='Mozilla/5.0 (Windows NT 5.1) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.872.0 Safari/535.2';
+$UaArr[]='Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.7 (KHTML, like Gecko) Chrome/16.0.912.36 Safari/535.7';
+$UaArr[]='Mozilla/5.0 (Windows NT 6.0; WOW64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.66 Safari/535.11';
+$UaArr[]='Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.45 Safari/535.19';
+$UaArr[]='Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24';
+$UaArr[]='Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1090.0 Safari/536.6';
+$UaArr[]='Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1';
+$UaArr[]='Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.15 (KHTML, like Gecko) Chrome/24.0.1295.0 Safari/537.15';
+$UaArr[]='Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.5; en-US; rv:1.9.1b3) Gecko/20090305 Firefox/3.1b3 GTB5';
+$UaArr[]='Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.5; ko; rv:1.9.1b2) Gecko/20081201 Firefox/3.1b2';
+$UaArr[]='Mozilla/5.0 (X11; U; SunOS sun4u; en-US; rv:1.9b5) Gecko/2008032620 Firefox/3.0b5';
+$UaArr[]='Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.8.1.12) Gecko/20080214 Firefox/2.0.0.12';
+$UaArr[]='Mozilla/5.0 (Windows; U; Windows NT 5.1; cs; rv:1.9.0.8) Gecko/2009032609 Firefox/3.0.8';
+$UaArr[]='Mozilla/5.0 (X11; U; OpenBSD i386; en-US; rv:1.8.0.5) Gecko/20060819 Firefox/1.5.0.5';
+$UaArr[]='Mozilla/5.0 (Windows; U; Windows NT 5.0; es-ES; rv:1.8.0.3) Gecko/20060426 Firefox/1.5.0.3';
+$UaArr[]='Mozilla/5.0 (Windows; U; WinNT4.0; en-US; rv:1.7.9) Gecko/20050711 Firefox/1.0.5';
+$UaArr[]='Mozilla/5.0 (Windows; Windows NT 6.1; rv:2.0b2) Gecko/20100720 Firefox/4.0b2';
+$UaArr[]='Mozilla/5.0 (X11; Linux x86_64; rv:2.0b4) Gecko/20100818 Firefox/4.0b4';
+$UaArr[]='Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.2) Gecko/20100308 Ubuntu/10.04 (lucid) Firefox/3.6 GTB7.1';
+$UaArr[]='Mozilla/5.0 (Windows NT 6.1; WOW64; rv:2.0b7) Gecko/20101111 Firefox/4.0b7';
+$UaArr[]='Mozilla/5.0 (Windows NT 6.1; WOW64; rv:2.0b8pre) Gecko/20101114 Firefox/4.0b8pre';
+$UaArr[]='Mozilla/5.0 (X11; Linux x86_64; rv:2.0b9pre) Gecko/20110111 Firefox/4.0b9pre';
+$UaArr[]='Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:2.0b9pre) Gecko/20101228 Firefox/4.0b9pre';
+$UaArr[]='Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:2.2a1pre) Gecko/20110324 Firefox/4.2a1pre';
+$UaArr[]='Mozilla/5.0 (X11; U; Linux amd64; rv:5.0) Gecko/20100101 Firefox/5.0 (Debian)';
+$UaArr[]='Mozilla/5.0 (Windows NT 6.1; WOW64; rv:6.0a2) Gecko/20110613 Firefox/6.0a2';
+$UaArr[]='Mozilla/5.0 (X11; Linux i686 on x86_64; rv:12.0) Gecko/20100101 Firefox/12.0';
+$UaArr[]='Mozilla/5.0 (Windows NT 6.1; rv:15.0) Gecko/20120716 Firefox/15.0a2';
+$UaArr[]='Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)';
+$UaArr[]='Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; Media Center PC 6.0; InfoPath.3; MS-RTC LM 8; Zune 4.7)';
+$UaArr[]='Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)';
+$UaArr[]='Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0)';
+$UaArr[]='Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Win64; x64; Trident/6.0)';
+$UaArr[]='Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0; GTB5; SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0; .NET CLR 3.0.04506; InfoPath.2; OfficeLiveConnector.1.3; OfficeLivePatch.0.0)';
+$UaArr[]='Mozilla/4.0 (Mozilla/4.0; MSIE 7.0; Windows NT 5.1; FDM; SV1; .NET CLR 3.0.04506.30)';
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $newURL);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_POST, true);
+
+$iUAID = rand (0, 51);
+$aUA = $UaArr[$iUAID];
+curl_setopt($ch, CURLOPT_USERAGENT, $aUA);//"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.64 Safari/537.31"
+
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+'Accept-Language: en-us,en;q=0.7,de-de;q=0.3',
+'Accept: text/xml,application/xml,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5',
+'X-Forwarded-For: '. $mainProxy));
+
+curl_setopt($ch, CURLOPT_ENCODING, 'gzip,deflate');
+curl_setopt($ch, CURLOPT_AUTOREFERER, true);
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 60);
+curl_setopt($ch, CURLOPT_POSTFIELDS, "countryE=countryE_$sCountry&codeCD=codeCD_%D4%CC$sFM&CodeSybbol=CodeSybbol_%C4&shortNum=$iDnum&DateTypeSearch=beginDate&day=day_$sDDay&mount=mount_$sDMonth&year=year_$sDYear&submit=+%C8%E4%E5%ED%F2%E8%F4%E8%F6%E8%F0%EE%E2%E0%F2%FC+&firstPage=false");
+//echo "countryE=countryE_$sCountry&codeCD=codeCD_%D4%CC$sFM&CodeSybbol=CodeSybbol_%C4&shortNum=$iDnum&DateTypeSearch=beginDate&day=day_$sDDay&mount=mount_$sDMonth&year=year_$sDYear&submit=+%C8%E4%E5%ED%F2%E8%F4%E8%F6%E8%F0%EE%E2%E0%F2%FC+&firstPage=false";
+
+$output = curl_exec($ch);
+$info = curl_getinfo($ch);
+curl_close($ch);
+$OutwithIfo['Code'] = $info['http_code'];
+$OutwithIfo['Time'] = $info['total_time'];
+$output = iconv("windows-1251", "UTF-8", $output);
+$output = str_replace(array("\r", "\n"), ' ',$output);
+
+$cutout2 ='';
+$constNF = 'Декларация не найдена';
+$constAD = 'За разъяснениями просим обращаться по e-mail: <a href=';
+$constCOR = "<font class='date'></font>";
+$constEnd = "<!-- Возможны новости -->";
+//==================Result check start======================
+if ($OutwithIfo['Code'] == 200) {
+    $sNotFound = stripos($output, $constNF);
+	if (stripos($output, $constCOR)==0) {
+	$OutwithIfo['ST'] = 'NODATA';};
+	if ($sNotFound  > 0){$OutwithIfo['ST'] = 'NOTFOUND';}else{
+	$OutwithIfo['ST'] = 'OK';
+	$output = str_replace("<!--//серия :seriesNumb:, партия :seriesVolume: :TypeName:, :BestDate:-->", "#TAG#", $output);
+	$cutout2 = substr($output, stripos($output, $constCOR), stripos($output, $constEnd) - stripos($output, $constCOR));
+	}
+	if (stripos($output, $constAD) > 0){$OutwithIfo['ST'] = 'DEADIP';}
+}
+$OutwithIfo['Html'] = $cutout2;
+
+echo json_encode($OutwithIfo);//$info['http_code'] . $sConstSepar . $info['total_time'] . $sConstSepar . $output;
+//echo $output;
 
 ?>
